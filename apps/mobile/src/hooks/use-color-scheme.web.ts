23bc572d2697afc04fb50763f85c6cd3f8aeef09
Wilfreno Gayongan <w.gayongan@gmail.com>
2026-07-13 08:43:07 +0800

    fix: remove usage of state inside useEffect

--- apps/mobile/src/hooks/use-color-scheme.web.ts
+++ apps/mobile/src/hooks/use-color-scheme.web.ts
@@ -7,10 +7,6 @@ import { useColorScheme as useRNColorScheme } from 'react-native';
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
 
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
-
   const colorScheme = useRNColorScheme();
 
   if (hasHydrated) {
